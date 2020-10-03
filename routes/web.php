070a735d8e109cc72237688a29475332ec3880e7
島田陸弥 <>
2020-10-03 17:42:12 +0900

    ルーティング

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('admin/profile/create', 'PrefileContlloer@add');
+    Route::get('admin/profile/edit', 'PrefileContlloer@edit');
+    
 });
